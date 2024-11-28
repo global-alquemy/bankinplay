960f30460cba814fb3c776bbf180d5a10adc0add
Javier de las Heras <jheras@alquemy.es>
2024-11-28 19:51:53 +0100

    [ADD] Callback

--- conciliation_online_bankinplay/models/callback.py
+++ conciliation_online_bankinplay/models/callback.py
@@ -7,14 +7,14 @@ _logger = logging.getLogger(__name__)
 
 class CallbackController(http.Controller):
 
-    @http.route('/webhook/estado', auth='public')
+    @http.route('/webhook/estado', auth='public', methods=['POST'], type='json')
     def callback_estado(self, **kw):
         params = request.env['ir.config_parameter'].sudo()
         _logger.info("Callback estado: %s", kw)
         return {}
     
-    @http.route('/webhook/lectura_intradia', auth='public')
-    def callback_estado(self, **kw):
+    @http.route('/webhook/lectura_intradia', auth='public', methods=['POST'], type='json')
+    def callback_lectura_intradia(self, **kw):
         params = request.env['ir.config_parameter'].sudo()
         _logger.info("Callback estado: %s", kw)
         return {}
