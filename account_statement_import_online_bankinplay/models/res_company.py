7d299d07b1858e715d9a45f388aeb02e77a2a6ef
José Antonio Fernández <jafernandez@alquemy.es>
2024-12-18 10:27:42 +0100

    [REFINE] Clean up whitespace and improve error messages in BankInPlay connection methods

--- account_statement_import_online_bankinplay/models/res_company.py
+++ account_statement_import_online_bankinplay/models/res_company.py
@@ -17,7 +17,7 @@ _logger = logging.getLogger(__name__)
 
 class ResCompany(models.Model):
     _inherit = "res.company"
-    
+
     bankinplay_apikey = fields.Char(
         string="Api Key",
         help="Key for BankInPlay API access.",
@@ -31,37 +31,37 @@ class ResCompany(models.Model):
     bankinplay_company_id = fields.Char(
         string="Company ID",
         help="Company ID for BankInPlay.",
-    )    
+    )
 
     def check_bankinplay_connection(self):
         if not self.bankinplay_apikey or not self.bankinplay_apisecret:
-            raise UserError(_("Please provide both the API Key and the API Secret."))
-        if not self.bankinplay_start_date:
-            raise UserError(_("Please provide the Banking Play Start Date."))
+            raise UserError(
+                _("Please provide both the API Key and the API Secret."))
         interface_model = self.env["bankinplay.interface"]
         access_data = interface_model._login(
             self.bankinplay_apikey, self.bankinplay_apisecret)
         if not access_data:
             raise UserError(_("Connection Test Failed!"))
-        
+
         return access_data
-    
+
     def test_bankinplay_connection(self):
-        
+
         access_data = self.check_bankinplay_connection()
-        
+
         interface_model = self.env["bankinplay.interface"]
         check_company = False
         get_companies = interface_model._get_companies(access_data)
         for company in get_companies:
-            if company['nif'] == self.vat.replace('ES', ''): 
+            if company['nif'] == self.vat.replace('ES', ''):
                 check_company = True
                 self.bankinplay_company_id = company['id']
                 break
-                
+
         if not check_company:
-            raise UserError(_("The company NIF does not match any of the companies in BankInPlay."))
-        
+            raise UserError(
+                _("The company NIF does not match any of the companies in BankInPlay."))
+
         title = _("Connection Test Succeeded!")
         message = _("Everything seems properly set up!")
         return {
@@ -73,4 +73,3 @@ class ResCompany(models.Model):
                 'sticky': False,
             }
         }
-    
