3336531aa27379456e1da33826abe4ec80a07668
José Antonio Fernández <jafernandez@alquemy.es>
2025-02-07 00:07:40 +0100

    UPDATE: Bump version to 15.0.2.0.9 for Online Bank Statements and Online Conciliation: BankInPlay

--- conciliation_online_bankinplay/__manifest__.py
+++ conciliation_online_bankinplay/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Online Conciliation: BankInPlay",
-    "version": "15.0.2.0.8",
+    "version": "15.0.2.0.9",
     "category": "Account",
     "author": "Alquemy",
     "website": "https://www.alquemy.es",
