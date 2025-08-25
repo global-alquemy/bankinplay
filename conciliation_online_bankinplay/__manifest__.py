b1655d34f8662a91b289f7c74aa3a52ca66c21d8
José Antonio Fernández <jafernandez@alquemy.es>
2025-08-25 13:10:14 +0200

    [UPDATE] Bump version to 16.0.1.0.0 and remove unused dependencies; refactor account move model and views

--- conciliation_online_bankinplay/__manifest__.py
+++ conciliation_online_bankinplay/__manifest__.py
@@ -4,17 +4,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Online Conciliation: BankInPlay",
-    "version": "14.0.2.0.0",
+    "version": "16.0.1.0.0",
     "category": "Account",
     "author": "Alquemy",
     "website": "https://www.alquemy.es",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["account_statement_import_online_bankinplay", "partner_manual_rank", "queue_job"],
+    "depends": ["account_statement_import_online_bankinplay", "partner_manual_rank"],
     "data": [
         "data/cron.xml",
         "views/res_company.xml",
-        "views/account_move_view.xml",
         "views/bank_statement.xml",
     ],
 }
